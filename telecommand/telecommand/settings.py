6d8ad0982566a04314a10ff3d2d0dae88e31fff2
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2018-05-08 21:22:10 +0100

    Hide key

--- telecommand/telecommand/settings.py
+++ telecommand/telecommand/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b)rhty0bb+jm^$0zcyvzsxsiqa8_$k#b1#t+xr$9-c8i%y7j-y'
+SECRET_KEY = secrets.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
