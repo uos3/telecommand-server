527eeed73922c84b1ea43e55a120d5db69499dfa
charlie-wt <cwt1g14@soton.ac.uk>
2018-05-08 18:19:09 +0100

    comments.

--- controllers/command.js
+++ controllers/command.js
@@ -4,14 +4,19 @@ exports.send_command = function (req, res) {
     const command = req.body.command;
 
     // verify validity of command
+    //  - lovely javascript bitwise operations probably
 
     // log command (w/ server timestamp)
+    //  - myqsl
 
     // get time of next cubesat pass from orbit prediction thing
+    //  - how to call & get the response?
 
     // get azumith & elevation of next cubesat pass from orbit prediction thing
+    //  - how to call & get the response?
 
-    // send data (how? in what form (binary?)? where to?)
+    // send data
+    //  - how? in what form (binary?)? where to?
 
     return res.send({ error: false, message: 'command sent!' });
 }
