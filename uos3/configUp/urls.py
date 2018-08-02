99d2e2fa30c0490fa3be8234ace07b7171995f06
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2018-08-02 19:47:01 +0100

    Ate that extra cruft

--- uos3/configUp/urls.py
+++ uos3/configUp/urls.py
@@ -6,12 +6,12 @@ app_name = 'configUp'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('configUp.html', views.ConfigUpView.as_view(), name='configUpd'),
-    path('configThanks.html', views.ThanksView.as_view(
+    path('configUp', views.ConfigUpView.as_view(), name='configUpd'),
+    path('configThanks', views.ThanksView.as_view(
         template_name="configUp/configThanks.html"), name='configThanks'),
-    path('listConfigs.html', views.ListConfigsView.as_view(), name='listConfigs'),
-    path('configDet.html/<int:pk>', views.ConfigUpView.as_view(
+    path('listConfigs', views.ListConfigsView.as_view(), name='listConfigs'),
+    path('configDet/<int:pk>', views.ConfigUpView.as_view(
         template_name="configUp/detail.html"), name='detail'),
-    path('delThanks.html', views.ThanksView.as_view(
+    path('delThanks', views.ThanksView.as_view(
         template_name="configUp/delThanks.html"), name='delThanks'),
 ]
