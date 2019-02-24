e5c106c1667d9a0e10d134e5f647e504ff929a1d
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2019-02-24 01:55:25 +0000

    Added user cred feature + login prompt

--- server-init/init.sh
+++ server-init/init.sh
@@ -2,6 +2,11 @@
 
 echo "Hi!\nInitiating UoS3 server config\n"
 
+USERS=$( cat "`dirname $0`/SECRETS/AUTH_USERS" )
+[ -z "$USERS" ] && {
+  echo "FATAL: You forgot to add the list of authorised users! Exiting..."
+  exit 1
+}
 SSL_DIR=$( cat "`dirname $0`/SECRETS/SSL_DIR" )
 [ -z $SSL_DIR ] && {
   echo "FATAL: You forgot to add the secret UoS3 SSL dir path! Exiting..."
@@ -17,18 +22,26 @@ SSL_DIR=$( cat "`dirname $0`/SECRETS/SSL_DIR" )
 if ! which nginx > /dev/null 2>&1; then
   echo "nginx is not installed! Installing...\n"
   sudo apt-get update && sudo apt-get -y install nginx
-fi
-if ! which nginx > /dev/null 2>&1; then
-  echo "\nFATAL: nginx is not installed! Exiting..."
-  exit 1
+  if ! which nginx > /dev/null 2>&1; then
+    echo "\nFATAL: nginx is not installed! Exiting..."
+    exit 1
+  fi
 fi
 if ! which openssl > /dev/null 2>&1; then
-  echo "OpenSSL is not installed! Installing...\n"
+  echo "OpenSSL required for secure connections. Installing...\n"
   sudo apt-get update && sudo apt-get -y install openssl
+  if ! which openssl > /dev/null 2>&1; then
+    echo "\nFATAL: OpenSSL is not installed! Exiting..."
+    exit 1
+  fi
 fi
-if ! which openssl > /dev/null 2>&1; then
-  echo "\nFATAL: OpenSSL is not installed! Exiting..."
-  exit 1
+if ! which htpasswd > /dev/null 2>&1; then
+  echo "apache2-utils needed for credential auth. Installing...\n"
+  sudo apt-get update && sudo apt-get -y install apache2-utils
+  if ! which htpasswd > /dev/null 2>&1; then
+    echo "\nFATAL: apache2-utils is not installed! Exiting..."
+    exit 1
+  fi
 fi
 
 if [ ! -d $SSL_DIR ]; then
@@ -40,6 +53,28 @@ if [ ! -d $SSL_DIR ]; then
 else
   echo "\nFound UoS3 SSL directory\n"
 fi
+
+if [ ! -f "${SSL_DIR}/.htpasswd" ]; then
+  echo "Cannot find user credential file. Creating..."
+  sudo touch "${SSL_DIR}/.htpasswd"
+  [ ! -f "${SSL_DIR}/.htpasswd" ] && {
+    echo "FATAL: Cannot create user credential file! Exiting..."; exit 1;
+  } || echo "Created\n"
+else
+  echo "Found user credential file\n"
+fi
+for user in $USERS; do
+  if ! grep -q "^$user" "${SSL_DIR}/.htpasswd"; then
+    echo "Credentials for user $user not found. Creating..."
+    sudo htpasswd ${SSL_DIR}/.htpasswd $user
+    if ! grep -q "^$user" "${SSL_DIR}/.htpasswd"; then
+      echo "\nFATAL: Cannot create credentials! Exiting..."
+      exit 1
+    else
+      echo "Created\n"
+    fi
+  fi
+done
 
 if [ ! -f "${SSL_DIR}/private/uos3.key" -o ! -f "${SSL_DIR}/certs/uos3.crt" ]; then
   echo "SSL cert is required. Creating..."
@@ -67,13 +102,21 @@ SNIP_CRT_POINT="${SSL_DIR}/snippets/uos3-cert-pointer.conf"
 sudo touch $SNIP_CRT_POINT
 echo "ssl_certificate ${SSL_DIR}/certs/uos3.crt" | sudo tee -a $SNIP_CRT_POINT
 echo "ssl_certificate_key ${SSL_DIR}/private/uos3.key" | sudo tee -a $SNIP_CRT_POINT
-[ ! "$SSL_STRONG_CONF" = "" ] && {
+[ ! -z "$SSL_STRONG_CONF" ] && {
   SNIP_STRONG_SSL="${SSL_DIR}/snippets/uos3-strong-ssl.conf"
   sudo touch $SNIP_STRONG_SSL
   cat $SSL_STRONG_CONF | sudo tee -a $SNIP_STRONG_SSL
 }
 echo "SSL snippets remade\n"
 
-echo "Enabling SSL in nginx config..."
+echo "Adding user credentials to nginx config..."
+
 echo "TODO"
+
+echo "Enabling SSL in nginx config..."
+
+echo "TODO"
+
 echo "Completed! Have a nice day!"
+echo "To check if the new nginx configs work, run 'sudo nginx -t'."
+echo "If everything is fine, then restart nginx."
